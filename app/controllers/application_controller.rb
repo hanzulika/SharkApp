27d2f5f9e345e5ece91b66d30349adf37ace5445
hanzu <hanzuioan5@gmail.com>
2026-02-14 16:17:53 +0200

    Fixed bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,5 @@ class ApplicationController < ActionController::Base
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
 
-  http_basic_authenticate_with name: "hanzu", password: "shark", except: [:index, :show]
+  http_basic_authenticate_with name: "hanzu", password: "shark", except: [ :index, :show ]
 end
